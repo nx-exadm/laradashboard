53bc7fc43e740b47619f875c6e53568ee3483041
nx-exadm <exadm@norielix.com>
2026-08-06 19:40:57 +0530

    Change sslmode from 'prefer' to 'require'

--- config/database.php
+++ config/database.php
@@ -75,7 +75,7 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'schema' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'require',
         ],
 
         'sqlsrv' => [
@@ -87,7 +87,7 @@ return [
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
-            'prefix' => '',
+            'prefix' => 'require',
             'prefix_indexes' => true,
         ],
 
