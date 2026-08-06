0d0c5e1ccd19b82aa3c6441fca501bf4fba715ca
nx-exadm <exadm@norielix.com>
2026-08-06 22:26:28 +0530

    Update TrustProxies to allow all proxies

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -1,18 +1,15 @@
 <?php
-
 namespace App\Http\Middleware;
-
 use Illuminate\Http\Middleware\TrustProxies as Middleware;
 use Illuminate\Http\Request;
-
 class TrustProxies extends Middleware
 {
     /**
      * The trusted proxies for this application.
      *
-     * @var array|string
+     * @var array|string|null
      */
-    protected $proxies;
+    protected $proxies = '*';
 
     /**
      * The headers that should be used to detect proxies.
